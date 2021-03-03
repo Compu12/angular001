adb6b8ecb83578ca27751d328fe7d7daf6248ec7
Compu12 <xavier.canizares.95@gmail.com>
2021-03-03 15:42:33 -0500

    data binging

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,8 +10,20 @@ export class AppComponent {
   diaSemana : string = "Martes";
   nombre:string= 'Xavier';
   apellido:string= 'Cañizares';
-
+  activado:boolean=true;
   cadenaTexto:string="Ingrese su nombre";  
+
+  rutaImagen:string= "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/1200px-Angular_full_color_logo.svg.png";
+
+//event binding
+  textoEventBinding:string="Este es un ejemplo de event Binding";
+  //two data binding
+  cadenaTwodataBinding:string='FISEI';
+  constructor (){
+    setInterval(()=>(this.activado=false),3000); //a los 3 segundos cambia de estado
+  }
+
+
   sumar(valor1:number, valor2: number){
   return valor1+valor2;
   }
@@ -19,4 +31,13 @@ export class AppComponent {
     return "Cadena de Texto"
   }
 
+  cambiarTexto():void{
+    this.textoEventBinding="El texto ha sido cambiado por un event binding";
+  }
+
+
+
+ 
+
 }
+
