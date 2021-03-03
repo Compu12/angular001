f81bd4eb679deb333a4c1e67ef40a1fd428be432
Compu12 <xavier.canizares.95@gmail.com>
2021-03-02 21:51:38 -0500

    data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular001';
+  diaSemana : string = "Martes";
+  nombre:string= 'Xavier';
+  apellido:string= 'Cañizares';
+
+  cadenaTexto:string="Ingrese su nombre";  
+  sumar(valor1:number, valor2: number){
+  return valor1+valor2;
+  }
+  mostrarMensaje(){
+    return "Cadena de Texto"
+  }
+
 }
